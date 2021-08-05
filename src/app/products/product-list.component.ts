92e80c4a2399d994f389d9b40406256c8bd94228
isabellesavannah <isabellet.208@gmail.com>
2021-08-05 09:47:50 -0600

    filter by working

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { filter } from "rxjs/operator/filter";
 import { IProduct } from "./Iproduct";
 
 
@@ -13,7 +14,17 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.performFilter(value);
+    }
+    
+    filteredProducts: IProduct[] = [];
     products: IProduct[] = [
         {
             "productId": 2,
@@ -36,6 +47,12 @@ export class ProductListComponent implements OnInit{
             "imageUrl": "assets/images/hammer.png"
           }
     ];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().includes(filterBy));
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
